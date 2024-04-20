b5a5926e04dcc6a08d477172c953876d0f003677
PotatoHakers <s.herza@mail.ru>
2024-04-20 19:56:54 +0300

    Update website style

--- AutEmplAcc/Pages/Index.cshtml.cs
+++ AutEmplAcc/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,44 +8,55 @@ using Microsoft.EntityFrameworkCore;
 using AutEmplAcc.Data;
 using AutEmplAcc.Model;
 
-namespace AutEmplAcc.Pages.Employees
+namespace AutEmplAcc.Pages.Emp
 {
-    public class IndexModel : PageModel
-    {
-        public Branches SelectedBranch { get; set; }
-        private readonly AutEmplAcc.Data.AutEmplAccContext _context;
+	public class IndexModel : PageModel
+	{
+		public Branches SelectedBranch { get; set; }
+		private readonly AutEmplAccContext _context;
 
-        public IndexModel(AutEmplAcc.Data.AutEmplAccContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(AutEmplAccContext context)
+		{
+			_context = context;
+		}
 
-        public IList<Employee> Employee { get; set; } = default!;
-        public List<Branches> Branches { get; set; }
+		public IList<Employee> Employee { get; set; } = default!;
+		public List<Branches> Branches { get; set; }
 
-        public async Task OnGetAsync()
-        {
+		public async Task OnGetAsync()
+		{
 
-            if (_context.Employees != null)
-            {
-                Employee = await _context.Employees.ToListAsync();
-            }
-            Branches = await _context.Branches.ToListAsync();
+			if (_context.Employees != null)
+			{
+				Employee = await _context.Employees.ToListAsync();
+			}
+			Branches = await _context.Branches.ToListAsync();
 
-        }
+		}
 
-        public async Task<IActionResult> OnPostShowEmployeesAsync(int branchId)
-        {
+		public async Task<IActionResult> OnPostShowEmployeesAsync(int branchId)
+		{
 
-            var branch = await _context.Branches
-        .Include(b => b.Employees)
-        .FirstOrDefaultAsync(b => b.BranchId == branchId);
-            if (branch != null)
-            {
-                return Partial("_EmployeeListPartial", branch.Employees);
-            }
-            return NotFound();
+			var branch = await _context.Branches
+		.Include(b => b.Employees)
+		.FirstOrDefaultAsync(b => b.BranchId == branchId);
+			if (branch != null)
+			{
+				return Partial("_EmployeeListPartial", branch.Employees);
+			}
+			return NotFound();
 
-        }
+		}
+
+        //public async Task<IActionResult> OnPostShowBranchesAsync(int branchId)
+        //{
+        //    var branch = await _context.Branches
+        //.FirstOrDefaultAsync(b => b.BranchId == branchId);
+        //    if (branch != null)
+        //    {
+        //        return Partial("_BranchListPartial", branch.Employees);
+        //    }
+        //    return NotFound();
+        //}
     }
 }

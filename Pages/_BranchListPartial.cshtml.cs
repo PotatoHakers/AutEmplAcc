0deefdf0d445d1c4495097d160b9fb9bc96a5738
PotatoHakers <s.herza@mail.ru>
2024-04-20 19:56:56 +0300

    Update website style

---
+++ Pages/_BranchListPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutEmplAcc.Pages
+{
+    public class _BranchListPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
